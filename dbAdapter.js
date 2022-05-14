4647d5698bd6d4d4c857aae590c4de1edd329bbf
nimrodbenari <105338215+nimrodbenari@users.noreply.github.com>
2022-05-14 19:27:12 +0300

    added css->index

--- dbAdapter.js
+++ dbAdapter.js
@@ -1,12 +1,11 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://EilonChabner:TimNimrodEilon1@cluster0.2978k.mongodb.net/test";
-
+var url = "mongodb+srv://EilonChabner:Eilonc618@cluster0.2978k.mongodb.net/test";
 
 
 var whatTodoNext = function(err, db) {
     if (err) throw err;
     var dbo = db.db("Products");
-    var myobj = { name: "beny", Age: "57" };
+    var myobj = { name: "Nimrod", Age: "29" };
     dbo.collection("Products2").insertOne(myobj, function(err, res) {
       if (err) throw err;
       console.log("1 document inserted");
