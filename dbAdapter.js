eac080a27ef24501af96990a167f49d89dd72897
nimrodbenari <105338215+nimrodbenari@users.noreply.github.com>
2022-05-16 17:06:37 +0300

    reg

--- dbAdapter.js
+++ dbAdapter.js
@@ -4,16 +4,27 @@ var url = "mongodb+srv://EilonChabner:TimNimrodEilon1@cluster0.2978k.mongodb.net
 
 var whatTodoNext = function(err, db) {
     if (err) throw err;
-    var dbo = db.db("Products");
+    var dbo = db.db("WaterSportCenter");
     var myobj = { Product: "shnorkel", price: "100" };
-    dbo.collection("Products2").insertOne(myobj, function(err, res) {
+    dbo.collection("customers").insertOne(myobj, function(err, res) {
       if (err) throw err;
       console.log("1 document inserted");
       db.close();
     });
     }
 
-
-
+function SendUser(name,email,hashedPassword) {
+  var dbo = db.db("WaterSportCenter");
+  var myobj = { user_name: name, Email: email,password:hashedPassword };
+  dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 customer inserted");
+    db.close();
+  });
+}
+  
 
 MongoClient.connect(url, whatTodoNext);
+MongoClient.connect(url, SendUser);
+exports.sendUser = SendUser;
+
