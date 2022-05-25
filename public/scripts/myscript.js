70413be3edf59e25bf1bffaa341cb63c12d494c8
nimrodbenari <105338215+nimrodbenari@users.noreply.github.com>
2022-05-25 09:52:03 +0300

    buy now

--- public/scripts/myscript.js
+++ public/scripts/myscript.js
@@ -14,12 +14,12 @@
     
     
     // Open and close sidebar
-    function w3_open() {
+    function func_open() {
       document.getElementById("mySidebar").style.display = "block";
       document.getElementById("myOverlay").style.display = "block";
     }
      
-    function w3_close() {
+    function func_close() {
       document.getElementById("mySidebar").style.display = "none";
       document.getElementById("myOverlay").style.display = "none";
     }
