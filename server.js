9b9023cca8481a65cd068c79c94a6e03091fe8f9
nimrodbenari <105338215+nimrodbenari@users.noreply.github.com>
2022-06-18 19:03:55 +0300

    table

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false}))
 app.use(express.static('public'))
-
+// db_adapter.getProducts();
+// db_adapter.getOrders();
 
 
 app.get('/login', (req, res) => {
-  res.render('login.html')
+  
+  res.render('login.html');
 })
 
 app.get('/register', (req, res) => {
@@ -30,18 +32,19 @@ app.post('/register.html', async (req, res) => {
 
 app.post('/login.html', async (req, res) => {
   try {
-   const hashedPassword = await bcrypt.hash(req.body.password, 10);
-   const result = db_adapter.checkUser(req.body.username,hashedPassword);
+  //  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+   const result = db_adapter.checkUser(req.body.username,req.body.password);
    if (result) {
+    // window.sessionStorage.setItem("username", result);
     res.redirect('/index.html');
    }
    else {
     res.redirect('/login.html')
     console.log('no customer was found')
    }
-  } catch  {
+  } catch (error) {
     res.redirect('/login.html')
-    console.log('no customer was found')
+    console.log(error)
   }
 }) 
 
@@ -52,7 +55,18 @@ app.post('/mangerpage.html', async (req, res) => {
   } catch  {
     res.redirect('/mangerpage.html')
   }
-}) 
+})
+
+
+
+app.get('/getproducts',(req, res) => {  
+  db_adapter.getProducts()
+  .then(function(response){
+    res.send(response)
+   })
+  })
+
+
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
