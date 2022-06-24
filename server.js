861c68af9ce89ca85666c99542a0d5e994dd6b4e
EilonChabner <chabner.eilon@gmail.com>
2022-06-24 17:21:06 +0300

    changed port to 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bcrypt = require('bcrypt')
 const db_adapter = require("./dbAdapter");
 const app = express();
-const port = 3000;
+const port = 8080;
 const bodyParser = require('body-parser');
 
 // ----------------APP USE-----------------------
